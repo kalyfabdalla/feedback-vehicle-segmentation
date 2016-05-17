a15a967cf7be81cf17c1cb21b1fbbaf947dfc873
Kalyf Abdalla <kalyfabdalla@gmail.com>
2016-05-17 07:35:28 -0300

    Working

---
+++ src/include/filtering.cpp
@@ -0,0 +1,92 @@
+extern "C"
+
+#include "filtering.hpp"
+
+BOOST_PYTHON_MODULE(Filtering)
+{
+    // Create the Python type object for our extension class and define __init__ function.
+    class_<Filtering>("Filtering",init<>())
+        .def("filter", &Filtering::filter)  // Add a regular member function.
+        .def("wrapFilter", &Filtering::wrapFilter)
+        .def("fillContours", &Filtering::fillContours)
+        .def("wrapFillContours", &Filtering::wrapFillContours)
+        .def("fillConvexHull", &Filtering::fillConvexHull)
+        .def("wrapFillConvexHull", &Filtering::wrapFillConvexHull)
+        ;
+}
+
+Filtering::Filtering()
+{
+
+}
+
+void Filtering::filter(unsigned char* image, unsigned char* output, int filter_flag, int cols, int rows)
+{
+  cv::Mat imageMat = cv::Mat(rows,cols,CV_8UC1,image);
+  cv::Mat outputMat = cv::Mat(rows,cols,CV_8UC1,output);
+
+  cv::medianBlur(imageMat, outputMat, 5);
+}
+
+void Filtering::fillContours(unsigned char* image, unsigned char* output, int value, int cols, int rows)
+{
+  cv::Mat imageMat = cv::Mat(rows,cols,CV_8UC1,image);
+  cv::Mat outputMat = cv::Mat(rows,cols,CV_8UC1,output);
+
+  findContours(imageMat,contours,hierarchy,CV_RETR_EXTERNAL,CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+  for (unsigned int i=0;i<contours.size();i++) {
+    int area = contourArea(contours[i]);
+    if (area>300){
+      drawContours(outputMat, contours, i, Scalar(value),CV_FILLED);
+    }
+  }
+}
+
+void Filtering::fillConvexHull(unsigned char* image, unsigned char* output, int value, int cols, int rows)
+{
+  cv::Mat imageMat = cv::Mat(rows,cols,CV_8UC1,image);
+  cv::Mat outputMat = cv::Mat(rows,cols,CV_8UC1,output);
+
+  findContours(imageMat,contours,hierarchy,CV_RETR_EXTERNAL,CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+  vector<vector<Point> >hull( contours.size() );
+  for(unsigned int i = 0; i < contours.size(); i++ ){
+    convexHull(Mat(contours[i]), hull[i], false );
+  }
+  for (unsigned int i=0;i<hull.size();i++) {
+    int area = contourArea(hull[i]);
+    if (area>300){
+      drawContours(outputMat, hull, i, Scalar(value),CV_FILLED);
+    }
+  }
+}
+
+void Filtering::wrapFilter(np::ndarray & image, np::ndarray & output, int filter_flag)
+{
+  this->filter(
+    reinterpret_cast<unsigned char*>(image.get_data()),
+    reinterpret_cast<unsigned char*>(output.get_data()),
+    filter_flag,
+    image.shape(1),
+    image.shape(0));
+}
+
+void Filtering::wrapFillContours(np::ndarray & image, np::ndarray & output, int value)
+{
+  this->fillContours(
+    reinterpret_cast<unsigned char*>(image.get_data()),
+    reinterpret_cast<unsigned char*>(output.get_data()),
+    value,
+    image.shape(1),
+    image.shape(0));
+}
+
+void Filtering::wrapFillConvexHull(np::ndarray & image, np::ndarray & output, int value)
+{
+  this->fillConvexHull(
+    reinterpret_cast<unsigned char*>(image.get_data()),
+    reinterpret_cast<unsigned char*>(output.get_data()),
+    value,
+    image.shape(1),
+    image.shape(0));
+}
---
+++ src/include/filtering.hpp
@@ -0,0 +1,45 @@
+#ifndef FILTERING_HPP_
+#define FILTERING_HPP_
+
+#define MEDIAN_FILTER 1
+#define GAUSSIAN_FILTER 2
+#define MEAN_FILTER 3
+
+#define BOOST_PYTHON_MAX_ARITY 25
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/opencv_modules.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+#include <cmath>
+#include <limits>
+#include <boost/python.hpp>
+#include <boost/numpy.hpp>
+
+using namespace cv;
+using namespace std;
+using namespace boost::python;
+
+namespace p = boost::python;
+namespace np = boost::numpy;
+
+class Filtering
+{
+private:
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+public:
+  Filtering();
+
+  void filter(unsigned char* image, unsigned char* output, int filter_flag, int cols, int rows);
+  void fillContours(unsigned char* image, unsigned char* output, int value, int cols, int rows);
+  void fillConvexHull(unsigned char* image, unsigned char* output, int value, int cols, int rows);
+  void wrapFilter(np::ndarray & image, np::ndarray & output, int filter_flag);
+  void wrapFillContours(np::ndarray & image, np::ndarray & output, int value);
+  void wrapFillConvexHull(np::ndarray & image, np::ndarray & output, int value);
+};
+
+#endif
